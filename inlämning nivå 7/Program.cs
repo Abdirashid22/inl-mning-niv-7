579521d0f7546b23219d2454f361204040188ff8
absa07002 <absa07002@DESKTOP-TAGHD5A>
2024-12-17 14:01:12 +0100

    Add project files.

---
+++ inlämning nivå 7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+
+        Console.WriteLine("Mata in positiva tal. Avsluta genom att mata in ett negativt tal.");
+        int tal;
+        
+        while (true)
+        {
+            Console.Write("Ange ett tal: ");
+            tal = int.Parse(Console.ReadLine());
+
+            if (tal < 0)
+            {
+                
+                break;
+            }
+        }
+
+        Console.WriteLine("Du matade in ett negativt tal. Programmet avslutas.");
+    }
+}
